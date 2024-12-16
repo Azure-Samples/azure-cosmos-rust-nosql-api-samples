a75fd3e3f1732e34b8866beb97e9244f586a0722
Theo van Kraay <theo.van@microsoft.com>
2024-12-16 13:27:44 +0000

    initial commit

---
+++ advanced-samples/cli/src/main.rs
@@ -0,0 +1,130 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+use azure_data_cosmos::CosmosClient;
+use azure_identity::DefaultAzureCredential;
+use clap::{Args, CommandFactory, Parser, Subcommand};
+use std::error::Error;
+
+mod create;
+mod delete;
+mod metadata;
+mod query;
+mod read;
+mod replace;
+mod upsert;
+
+/// An example which shows how you might create a CLI tool for managing items in Cosmos DB using the Rust SDK.
+///
+/// NOTE: This is not intended to be a general-purpose CLI for managing items in Cosmos DB.
+/// It exists for illustrative purposes, and could also be used to simplify ad-hoc end-to-end testing.
+///
+/// usage examples when running from command line:
+///
+///
+/// Note: database must already exist as database creation is not supported when using RBAC or DefaultAzureCredential.
+/// If you are using a service principal that did not create the account, you must also create containers using the Azure Portal or the Azure CLI.
+///
+/// First sign in using az login, then set the subscription and resource group.
+///
+/// Set an environment variable for your Cosmos DB endpoint: COSMOSDB_ENDPOINT
+///
+/// For windows (use "$COSMOSDB_ENDPOINT" for Linux):
+///
+/// create container:
+///
+/// cargo run -- "%COSMOSDB_ENDPOINT%" create container database-name --id container-name --partition-key "/id"
+///
+/// create item:
+///
+/// cargo run -- "%COSMOSDB_ENDPOINT%" create item database-name container-name --partition-key "my-item-id" --json "{\"id\": \"my-item-id\", \"name\": \"my-item-name\"}"
+///
+/// upsert item:
+///
+/// cargo run -- "%COSMOSDB_ENDPOINT%" upsert --partition-key "my-item-id" --json "{\"id\": \"my-item-id\", \"name\": \"my-item-name-changed\"}" database-name container-name
+///
+/// read item:
+///
+/// cargo run -- "%COSMOSDB_ENDPOINT%" read database-name container-name --item-id "my-item-id" --partition-key "my-item-id"
+///
+/// query items:
+///
+/// cargo run -- "%COSMOSDB_ENDPOINT%" query items database-name container-name "select * from c" --partition-key "my-item-id"
+///
+/// delete item:
+///
+/// cargo run -- "%COSMOSDB_ENDPOINT%" delete item database-name container-name --item-id "my-item-id" --partition-key "my-item-id"
+
+///
+#[derive(Clone, Parser)]
+struct ProgramArgs {
+    #[clap(flatten)]
+    shared_args: SharedArgs,
+
+    #[command(subcommand)]
+    subcommand: Option<Subcommands>,
+}
+
+#[derive(Clone, Args)]
+struct SharedArgs {
+    /// The Cosmos DB endpoint to connect to.
+    endpoint: String,
+
+    /// An authentication key to use when connecting to the Cosmos DB account. If omitted, the connection will use Entra ID.
+    #[clap(long)]
+    key: Option<String>,
+}
+
+#[derive(Clone, Subcommand)]
+enum Subcommands {
+    Create(create::CreateCommand),
+    Delete(delete::DeleteCommand),
+    Metadata(metadata::MetadataCommand),
+    Query(query::QueryCommand),
+    Read(read::ReadCommand),
+    Replace(replace::ReplaceCommand),
+    Upsert(upsert::UpsertCommand),
+}
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .init();
+
+    let args = ProgramArgs::parse();
+
+    let Some(cmd) = args.subcommand else {
+        ProgramArgs::command().print_long_help()?;
+        return Ok(());
+    };
+
+    let client = create_client(&args.shared_args)?;
+
+    match cmd {
+        Subcommands::Create(cmd) => cmd.run(client).await,
+        Subcommands::Delete(cmd) => cmd.run(client).await,
+        Subcommands::Metadata(cmd) => cmd.run(client).await,
+        Subcommands::Query(cmd) => cmd.run(client).await,
+        Subcommands::Read(cmd) => cmd.run(client).await,
+        Subcommands::Replace(cmd) => cmd.run(client).await,
+        Subcommands::Upsert(cmd) => cmd.run(client).await,
+    }
+}
+
+fn create_client(args: &SharedArgs) -> Result<CosmosClient, Box<dyn Error>> {
+    if let Some(key) = args.key.as_ref() {
+        #[cfg(feature = "key_auth")]
+        {
+            Ok(CosmosClient::with_key(&args.endpoint, key.clone(), None)?)
+        }
+        #[cfg(not(feature = "key_auth"))]
+        {
+            let _ = key; // Mark 'key' as used to make the compiler happy.
+            Err("cannot authenticate with a key unless the 'key_auth' feature is enabled".into())
+        }
+    } else {
+        let cred = DefaultAzureCredential::new().unwrap();
+        Ok(CosmosClient::new(&args.endpoint, cred, None)?)
+    }
+}
---
+++ basic-samples/document-crud/src/main.rs
@@ -0,0 +1,156 @@
+use azure_core::StatusCode;
+use azure_data_cosmos::{CosmosClient, PartitionKey};
+use azure_identity::DefaultAzureCredential;
+use futures::StreamExt;
+use serde_json::json;
+use std::env;
+use tokio;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Replace with your actual Cosmos DB credentials
+
+    // Load cosmos db environment variables
+    match env::var("COSMOSDB_ENDPOINT") {
+        Ok(value) => println!("COSMOSDB_ENDPOINT is: {}", value),
+        Err(e) => println!("Couldn't read COSMOSDB_ENDPOINT ({})", e),
+    }
+
+    let endpoint = env::var("COSMOSDB_ENDPOINT").unwrap();
+    let credential = DefaultAzureCredential::new().unwrap();
+
+    // Create a Cosmos client
+    let client = CosmosClient::new(endpoint, credential, None)?;
+
+    // Set database (database must already exist, create database not support in RBAC)
+    let database = "database-name";
+
+    // Set container (ensure this container already exists with partition key of "/pk")
+    let container = "container-name";
+
+    let db_client = client.database_client(database);
+    let container_client = db_client.container_client(container);
+
+    let items = vec![
+        json!({
+            "id": "my-item-id-value-1",
+            "pk": "my-item-partition-key-value",
+            "name": "my-item-name-1",
+        }),
+        json!({
+            "id": "my-item-id-value-2",
+            "pk": "my-item-partition-key-value",
+            "name": "my-item-name-2",
+        }),
+    ];
+
+    for item in items {
+        let item_id = item["id"].clone();
+        match container_client
+            .create_item("my-item-partition-key-value", item, None)
+            .await
+        {
+            Ok(create_response) => {
+                let created_item = create_response.deserialize_body().await?;
+                println!("Created item: {:#?}", created_item);
+            }
+            Err(e) if e.http_status() == Some(StatusCode::Conflict) => {
+                return Err(format!("Document with ID '{}' already exists.", item_id).into());
+            }
+            Err(e) => return Err(e.into()),
+        }
+    }
+
+    // read item
+    let read_response = container_client
+        .read_item("my-item-partition-key-value", "my-item-id-value-1", None)
+        .await;
+    match read_response {
+        Err(e) if e.http_status() == Some(StatusCode::NotFound) => println!("Item not found!"),
+        Ok(r) => {
+            let item: serde_json::Value = r.deserialize_body().await?.unwrap();
+            println!("Found item:");
+            println!("{:#?}", item);
+        }
+        Err(e) => return Err(e.into()),
+    };
+
+    // query items
+    let query = "SELECT * FROM c";
+
+    let db_client = client.database_client(&database);
+    let container_client = db_client.container_client(&container);
+
+    let pk = PartitionKey::from("my-item-partition-key-value");
+    let mut items =
+        container_client.query_items::<serde_json::Value>(&query.to_string(), pk, None)?;
+
+    while let Some(page) = items.next().await {
+        let page = page?.deserialize_body().await?;
+        println!("Query results page");
+        println!("  Items:");
+        for item in page.items {
+            println!("    * {:#?}", item);
+        }
+    }
+
+    // upsert item
+    let upsert_item = serde_json::json!({
+        "id": "my-item-id-value-1",
+        "pk": "my-item-partition-key-value",
+        "name": "my-item-name-updated",
+    });
+    let upsert_response = container_client
+        .upsert_item("my-item-partition-key-value", upsert_item, None)
+        .await?
+        .deserialize_body()
+        .await?
+        .unwrap();
+    println!("Upsert item:");
+    println!("{:#?}", upsert_response);
+
+    // replace item
+
+    let replace_item = serde_json::json!({
+        "id": "my-item-id-value-2",
+        "pk": "my-item-partition-key-value",
+        "name": "my-item-name-replaced",
+    });
+    let replace_response = container_client
+        .replace_item(
+            "my-item-partition-key-value",
+            "my-item-id-value-2",
+            replace_item,
+            None,
+        )
+        .await?
+        .deserialize_body()
+        .await?
+        .unwrap();
+    println!("Replace item:");
+    println!("{:#?}", replace_response);
+
+    // delete items
+    let item_ids = vec!["my-item-id-value-1", "my-item-id-value-2"];
+
+    for item_id in item_ids {
+        let delete_response = container_client
+            .delete_item("my-item-partition-key-value", item_id, None)
+            .await;
+
+        match delete_response {
+            Err(e) if e.http_status() == Some(StatusCode::NotFound) => {
+                println!("Item with ID '{}' not found!", item_id);
+            }
+            Ok(_) => {
+                println!("Item with ID '{}' deleted", item_id);
+            }
+            Err(e) => {
+                eprintln!("Error deleting item with ID '{}': {}", item_id, e);
+                return Err(e.into()); // Stop on the first error encountered
+            }
+        }
+    }
+
+    Ok(())
+}
